a3e985a2a25685e1ff8e4bf679c5a84b562e1483
Guilherme Jr. <falecom@guilhermejr.net>
2024-03-10 11:44:22 -0300

    fix: removido importe desnecessário

--- src/main/java/net/guilhermejr/sistema/energia/service/AcompanhamentoService.java
+++ src/main/java/net/guilhermejr/sistema/energia/service/AcompanhamentoService.java
@@ -22,7 +22,6 @@ import java.math.RoundingMode;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
